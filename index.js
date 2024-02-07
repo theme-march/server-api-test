aaace42174af9f763f8f469428835bbf8d27eb1c
theme-march <secure.storerepublic@gmail.com>
2024-02-07 16:46:02 +0600

    fiedsdas

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hr7oi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = process.env.DBHOST;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
